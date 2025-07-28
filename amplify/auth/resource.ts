46a694174e2df85126ae04534fbf53f467eea41e
thanhtrungtran <trantrungthanh910@gmail.com>
2025-07-28 21:16:26 +0700

    Remove the auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,5 +3,11 @@ import { defineAuth } from "@aws-amplify/backend"
 export const auth = defineAuth({
     loginWith: {
         email: true,
+    },
+    userAttributes: {
+        preferredUsername: {
+        mutable: true,
+        required: false
+        }
     }
 })
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.ts';
 import { data } from './data/resource.ts';
 
 defineBackend({
-    auth,
     data,
 });
