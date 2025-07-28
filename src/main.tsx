d6a1acc0503a49af3dc48b41c43897d515718861
thanhtrungtran <trantrungthanh910@gmail.com>
2025-07-28 19:17:15 +0700

    revert back the code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Authenticator } from '@aws-amplify/ui-react';
-//import { Amplify } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-//import outputs from "../amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import './index.css'
 import App from './main_app/App.tsx'
 import Header from './header/Header.tsx'
-//Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 
 createRoot(document.getElementById('root')!).render(
